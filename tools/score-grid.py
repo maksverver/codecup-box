3fe0a36f42248365ef0db556fc9043541f601313
Maks Verver <maks@verver.ch>
2024-09-06 14:26:16 +0200

    Make comment stripping a bit more general
    Just out of principle. This allows comments to appear in/on the first line too.

--- tools/score-grid.py
+++ tools/score-grid.py
@@ -4,11 +4,14 @@ import sys
 
 import box
 
-def StripComment(s):
-    i = s.find('#')
-    if i >= 0: s = s[:i]
-    return s
-
+# Returns a generator that removes comments from lines (starting with '#'),
+# strips trailing whitespace, and yields only the nonempty resulting lines.
+def StripComments(file):
+    for line in file:
+        i = line.find('#')
+        if i >= 0: line = line[:i]
+        line = line.rstrip()
+        if line != '': yield line
 
 def Main():
     if len(sys.argv) > 2:
@@ -16,12 +19,11 @@ def Main():
         sys.exit(1)
 
     with (open(sys.argv[1], 'rt') if len(sys.argv) == 2 else sys.stdin) as file:
-        secret_colors = [int(word) for word in file.readline().split()]
+        lines = StripComments(file)
+        secret_colors = list(map(int, next(lines).split()))
         assert len(secret_colors) == 2
         board = None
-        for line in file:
-            line = StripComment(line).strip()
-            if line == '': continue
+        for line in lines:
             tile, placement = box.ParseTilePlacement(line)
             if board is None:
                 board = box.BoardState(tile, placement)
