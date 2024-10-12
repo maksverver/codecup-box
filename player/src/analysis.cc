6bb7aa2584b9603ab3a1878b9a9ef0e5d5659198
Maks Verver <maks@verver.ch>
2024-10-12 19:52:34 +0200

    Bugfix: don't overcount horizontal pairs of matching cells.
    This change makes the player slightly weaker. This can be fixed later by
    adjusting the weights of the evaluation function (increasing the value of
    horizontal pairs of cells).

--- player/src/analysis.cc
+++ player/src/analysis.cc
@@ -57,50 +57,58 @@ grid_t CalcFixed(const grid_t &grid) {
   return fixed;
 }
 
+static int EvaluateRectangle(const grid_t &grid, const grid_t &fixed, color_t color, int r1, int c1, int r2, int c2) {
+  //  a  b
+  //  c  d
+  bool a = grid[r1][c1] == color;
+  bool b = grid[r1][c2] == color;
+  bool c = grid[r2][c1] == color;
+  bool d = grid[r2][c2] == color;
+  bool fa = fixed[r1][c1];
+  bool fb = fixed[r1][c2];
+  bool fc = fixed[r2][c1];
+  bool fd = fixed[r2][c2];
+  int size = r2 - r1;
+  int num_fixed = fa + fb + fc + fd;
+  int score = 0;
+  if (a && b && c && d) {
+    // Square!
+    score += 1000 + 100*num_fixed + 100*size;
+  } else if (
+        (a && b && c && !fd) ||
+        (a && b && d && !fc) ||
+        (a && c && d && !fb) ||
+        (b && c && d && !fa)) {
+    // One cell short of a square.
+    score += 100 + 10*num_fixed + 10*size;
+  } else if (
+      (a && b && !fc && !fd) ||
+      (a && c && !fb && !fd) ||
+      (a && d && !fb && !fc) ||
+      (b && c && !fa && !fd)
+    // These are excluded to avoid double-processing as edges a-b and a-c
+    // (b && d && !fa && !fc) ||
+    // (c && d && !fa && !fb)
+  ) {
+    // Two points aligned horizontally, vertically, or diagonally.
+    // Maybe: assign a different score for the diagonal version?
+    score += 10 + 1*num_fixed + 1*size;
+  }
+  return score;
+}
+
 void EvaluateAllColors(const grid_t &grid, const grid_t &fixed, std::array<int, COLORS> &scores) {
   scores = {};
   for (int color = 1; color <= COLORS; ++color) {
     int score = 0;
     for (int r1 = 0; r1 < HEIGHT; ++r1) {
       for (int c1 = 0; c1 < WIDTH; ++c1) {
-        //  a  b
-        //  c  d
-        bool a = grid[r1][c1] == color;
-        bool fa = fixed[r1][c1];
-        if (a) {
+        if (grid[r1][c1] == color) {
           // Always assign a point to each cell.
           score += 1;
         }
         for (int r2 = r1 + 1, c2 = c1 + 1; r2 < HEIGHT && c2 < WIDTH; ++r2, ++c2) {
-          int size = r2 - r1;
-          bool b = grid[r1][c2] == color;
-          bool c = grid[r2][c1] == color;
-          bool d = grid[r2][c2] == color;
-          bool fb = fixed[r1][c2];
-          bool fc = fixed[r2][c1];
-          bool fd = fixed[r2][c2];
-          int num_fixed = fa + fb + fc + fd;
-          if (a && b && c && d) {
-            // Square!
-            score += 1000 + 100*num_fixed + 100*size;
-          } else if (
-                (a && b && c && !fd) ||
-                (a && b && d && !fc) ||
-                (a && c && d && !fb) ||
-                (b && c && d && !fa)) {
-            // One cell short of a square.
-            score += 100 + 10*num_fixed + 10*size;
-          } else if (
-              (a && b && !fc && !fd) ||
-              (a && c && !fb && !fd) ||
-              (a && d && !fb && !fc) ||
-              (b && c && !fa && !fd) ||
-              (b && d && !fa && !fc) ||
-              (c && d && !fa && !fb)) {
-            // Two points aligned horizontally, vertically, or diagonally.
-            // Maybe: assign a different score for the diagonal version?
-            score += 10 + 1*num_fixed + 1*size;
-          }
+          score += EvaluateRectangle(grid, fixed, color, r1, c1, r2, c2);
         }
       }
     }
